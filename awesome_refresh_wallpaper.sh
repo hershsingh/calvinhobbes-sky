552614b6f5a1312f0c0d5ae4b9b793b9433462e6
Hersh Singh <hershdeep@gmail.com>
2014-11-17 16:30:28 -0500

    Add script to refresh AwesomeWM wallpaper

---
+++ awesome_refresh_wallpaper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Hersh Singh [hershdeep at gmail dot com]
+# Description: Refreshes the wallpaper for Awesome window manager.
+
+echo "gears.wallpaper.maximized(theme.wallpaper, 1, true)" |awesome-client
