057b7a88264f07f4f1c116d2158f1622bc75da5d
Hersh Singh <hershdeep@gmail.com>
2014-11-17 14:27:43 -0500

    Add some error handling.

--- space.sh
+++ space.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Exit if any command fails
+set -e
+
 # Parameters
 resolution=1600x900
 img_src=sky/sky0.jpg
-img_output=background.png
 img_calvinhobbes=calvinhobbes_clear.png
+img_output=background.png
 
 # Intermediate Images
 img_black=black.png
@@ -12,6 +15,16 @@ img_gradient=gradient.png
 img_src_resized=source.png
 img_sky_transparent=sky_transparent.png
 img_sky_blended=sky_blended.png
+
+# Check if the required files exist
+if [ ! -f $img_src ]; then 
+    echo Error: The sky image file is missing. 
+    exit 1
+fi
+if [ ! -f $img_calvinhobbes ]; then 
+    echo Error: Calvin and Hobbes overlay image file is missing.
+    exit 1
+fi
 
 # Make Gradient
 if [ ! -f $img_gradient ]; then
