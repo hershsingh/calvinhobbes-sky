fb5c91a28ca2957b81824f4bd8423692c81fba8c
Hersh Singh <hershdeep@gmail.com>
2014-11-17 13:53:27 -0500

    Initial commit.

---
+++ space.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Parameters
+resolution=1600x900
+img_src=sky/sky0.jpg
+img_output=background.png
+img_calvinhobbes=c_clear_lower.png
+
+# Intermediate Images
+img_black=black.png
+img_gradient=gradient.png
+img_src_resized=source.png
+img_sky_transparent=sky_transparent.png
+img_sky_blended=sky_blended.png
+
+# Make Gradient
+if [ ! -f $img_gradient ]; then
+    echo "Creating gradient..."
+    convert  -alpha Off -size $resolution gradient: -sigmoidal-contrast 10,30% $img_gradient
+fi
+if [ ! -f $img_black ]; then
+    echo "Creating black backdrop..."
+    convert  -alpha Off -size $resolution xc:black $img_black
+fi
+
+# Resize sky image
+convert -alpha Off -resize $resolution\! $img_src $img_src_resized
+
+# Use the gradient as an opacity mask to fade the sky image into zero opacity
+convert $img_src_resized $img_gradient -alpha Off \
+        -compose CopyOpacity -composite $img_sky_transparent
+# Compose the transparent sky with black background 
+convert $img_black $img_sky_transparent -gravity South -composite $img_sky_blended
+# Paste Calvin & Hobbes on top of the background
+convert $img_sky_blended $img_calvinhobbes -gravity South -composite $img_output
